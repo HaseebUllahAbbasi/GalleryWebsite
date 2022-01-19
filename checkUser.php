433a0c4a39d00fb5d71a01b10f71e9e50ec6a7aa
Haseeb U <haseebabbasi00@gmail.com>
2022-01-19 14:04:43 +0500

    php checking user login

--- checkUser.php
+++ checkUser.php
@@ -1,6 +1,33 @@
-<?php 
-    $userName = $_GET["userName"];
-    $password = $_GET["password"];
-    $typeOfUser = $_GET["typeOfUser"];
-      
+
+<?php
+
+require_once "connection.php";
+
+
+$userName = $_GET["userName"];
+$password = $_GET["password"];
+$typeOfUser = $_GET["typeOfUser"];
+
+$connect = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+
+if ($typeOfUser == "user") {
+    echo "User is About to Login";
+} else if ($typeOfUser == "photographer") {
+    echo "photographer is About to Login";
+} else {
+    echo "Admin  is About to Login";
+    // $sql = `SELECT * FROM ${TABLE_ADMIN}  WHERE User = '$user_1'`;
+
+    $sql = "SELECT * FROM admin_table where name = '$userName' and password = '${password}' ";
+    $result = $connect->query($sql);
+    $rows = $result->num_rows;
+    if ($rows == 1) {
+        echo "<script> alert(' You Have Logined ');</script>";
+        // header("Location: yes_lite.php");
+    } else {
+        echo "<script> alert(' You Have Not Logined ');</script>";
+    }
+}
+
 ?>
--- connection.php
+++ connection.php
@@ -5,6 +5,13 @@ define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'gallery');
 
+define('TABLE_USER', 'user_table');
+define('TABLE_RATING', 'rating_table');
+define('TABLE_PHOTOS', 'photostable');
+define('TABLE_CONTEST', 'contest');
+define('TABLE_VOTE', 'vote_table');
+define('TABLE_ADMIN', 'admin_table');
+define('TABLE_PARTICIPANT', 'participanttable');
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME); 
 if($link === false)
 {
